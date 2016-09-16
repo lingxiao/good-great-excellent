72edfc36aec028a9ff547e0db8135e70a50cdad6
Xiao Ling <lingxiao@seas.upenn.edu>
2016-09-16 11:25:56 -0400

    rename subroutines

--- .stack-work/dist/x86_64-osx/Cabal-1.22.5.0/build/autogen/Paths_good_great_excellent.hs
+++ .stack-work/dist/x86_64-osx/Cabal-1.22.5.0/build/autogen/Paths_good_great_excellent.hs
@@ -17,7 +17,7 @@ version = Version [0,1,0,0] []
 bindir, libdir, datadir, libexecdir, sysconfdir :: FilePath
 
 bindir     = "/Users/lingxiao/Documents/research/code/good-great-excellent/.stack-work/install/x86_64-osx/lts-6.11/7.10.3/bin"
-libdir     = "/Users/lingxiao/Documents/research/code/good-great-excellent/.stack-work/install/x86_64-osx/lts-6.11/7.10.3/lib/x86_64-osx-ghc-7.10.3/good-great-excellent-0.1.0.0-H90TC5DRfXfJF7pQf276VX"
+libdir     = "/Users/lingxiao/Documents/research/code/good-great-excellent/.stack-work/install/x86_64-osx/lts-6.11/7.10.3/lib/x86_64-osx-ghc-7.10.3/good-great-excellent-0.1.0.0-GBdY7oRp6g78bKpCLQAiTZ"
 datadir    = "/Users/lingxiao/Documents/research/code/good-great-excellent/.stack-work/install/x86_64-osx/lts-6.11/7.10.3/share/x86_64-osx-ghc-7.10.3/good-great-excellent-0.1.0.0"
 libexecdir = "/Users/lingxiao/Documents/research/code/good-great-excellent/.stack-work/install/x86_64-osx/lts-6.11/7.10.3/libexec"
 sysconfdir = "/Users/lingxiao/Documents/research/code/good-great-excellent/.stack-work/install/x86_64-osx/lts-6.11/7.10.3/etc"
