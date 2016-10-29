e4c24b810602e77096eb2391814deda696845dfa
Xiao Ling <lingxiao@seas.upenn.edu>
2016-10-29 18:56:36 -0400

    stuff

--- milp/readData.py
+++ milp/readData.py
@@ -8,21 +8,51 @@ import os
 
 ############################################################
 # Project Text File Config
+############################################################
 
-patts = "/Users/lingxiao/Documents/research/code/good-great-excellent/inputs/"
-p_ws  = os.path.join(patts,"strong-weak-patterns.txt")
-p_sw  = os.path.join(patts,"weak-strong-patterns.txt")
-path  = "/Users/lingxiao/Documents/research/code/good-great-excellent/out/word-count/good-bad-weak-strong"
-ai    = "good"
-aj    = "better"
+
+# @USE: Given project configurations:
+# 			- directory to linguistic patterns
+# 			- directory to strong-weak pattern counts
+# 			- directory to weak-strong pattern counts
+# Output CONFIG mapping name to directories if all directories
+# valid. else throw error
+
+# config :: DirectoryPath -> DirectoryPath -> DirectoryPath 
+#        -> Dict String DirectoryPath
+def config(pattern, weakStrong, strongWeak):
+
+	if os.path.isdir(patternDir) and \
+	   os.path.isdir(weakStrong) and \
+	   os.path.isdir(strongWeak):
+
+		return { "patterns"    : patternDir \
+	      	   , "weak-strong" : weakStrongDir \
+	      	   , "strong-weak" : strongWeakDir}
+	else:
+		raise NameError("invalid directory: ", pattern, weakStrong, strongWeak)
+
+patternDir    = "/Users/lingxiao/Documents/research/code/good-great-excellent/inputs/"
+strongWeakDir = "/Users/lingxiao/Documents/research/code/good-great-excellent/out/strong-weak-words"
+weakStrongDir = "/Users/lingxiao/Documents/research/code/good-great-excellent/out/weak-strong-words"
+
+ai  = "good"
+aj  = "better"
+CON = config(patternDir, weakStrongDir, strongWeakDir)
+
+############################################################
+# Populate word statistics
+############################################################
 
 # @Use: Given adjectives `ai`  and `aj`, and filepath `path`
 # output 
 
-# popuulate :: String -> String -> FilePath -> IO Float
-def populate(ai,aj,path):
+# populate :: FilePath -> String -> String -> IO Float
+def populate(path,ai,aj):
 	xs   = readFile(path, ai + "-" + aj)
 	return xs
+
+
 
 
 
@@ -33,16 +63,16 @@ def populate(ai,aj,path):
 # @Use: given `path` to directory holding strong-weak-pattern
 # read pattern and output as list
 
-# toStrongWeak :: DirectoryPath -> Eff [IO, Error String] [String]
-def toStrongWeak(path):
-	return readFile(path, 'strong-weak-patterns')
+# toStrongWeak :: CON -> Eff [IO, Error String] [String]
+def toStrongWeak(con):
+	return readFile(con['patterns'], 'strong-weak-patterns')
 
 # @Use: given `path` to directory holding weak-strong-pattern
 # read pattern and output as list
 
-# toWeakStrong :: DirectoryPath -> Eff [IO, Error String] [String]
-def toWeakStrong(path):
-	return readFile(path, 'weak-strong-patterns')
+# toWeakStrong :: CON -> Eff [IO, Error String] [String]
+def toWeakStrong(con):
+	return readFile(con['patterns'], 'weak-strong-patterns')
 
 
 ############################################################
@@ -54,7 +84,9 @@ def toWeakStrong(path):
 # and output as list of lines
 # Throw exception if file Not Found
 
-# readFile :: DirectoryPath -> String -> Eff [IO, Error String] [String]
+# readFile :: CONFIG
+#          -> DirectoryPath 
+#          -> Eff [IO, Error String] [String]
 def readFile(path,name):
 	path = os.path.join(path,name + ".txt")
 	if os.path.isfile(path):
